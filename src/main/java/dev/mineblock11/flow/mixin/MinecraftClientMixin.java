752328e7a7549653432a5b347d2dc6065624279e
Calum <contact@mineblock11.dev>
2023-10-26 16:28:40 +0100

    fix: DISC-7

---
+++ src/main/java/dev/mineblock11/flow/mixin/MinecraftClientMixin.java
@@ -0,0 +1,20 @@
+package dev.mineblock11.flow.mixin;
+
+import dev.mineblock11.flow.api.WindowResizeEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Shadow @Final private Window window;
+    @Inject(method = "onResolutionChanged", at = @At("TAIL"))
+    private void $window_resize_invoker(CallbackInfo ci) {
+        WindowResizeEvent.EVENT.invoker().invoke(this.window.getFramebufferWidth(), this.window.getFramebufferHeight());
+    }
+}
--- src/main/java/dev/mineblock11/flow/render/BlurHelper.java
+++ src/main/java/dev/mineblock11/flow/render/BlurHelper.java
@@ -1,6 +1,7 @@
 package dev.mineblock11.flow.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import dev.mineblock11.flow.api.WindowResizeEvent;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.gl.GlUniform;
@@ -23,9 +24,11 @@ public class BlurHelper {
     private Framebuffer input;
     private ShaderProgram backingProgram;
 
-    public void onWindowResize(MinecraftClient client, Window window) {
-        if (this.input == null) return;
-        this.input.resize(window.getFramebufferWidth(), window.getFramebufferHeight(), MinecraftClient.IS_SYSTEM_MAC);
+    public BlurHelper() {
+        WindowResizeEvent.EVENT.register((width, height) -> {
+            if (this.input == null) return;
+            this.input.resize(width, height, MinecraftClient.IS_SYSTEM_MAC);
+        });
     }
 
     public void load(ShaderProgram backingProgram) {
