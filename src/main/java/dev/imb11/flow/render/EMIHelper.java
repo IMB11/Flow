a5ae239cefaea6ed7cd5c14ac1d14e84f2b19c2e
Calum <contact@mineblock11.dev>
2024-06-27 21:22:40 +0100

    fix: Stonecutter bump

--- src/main/java/dev/imb11/flow/render/EMIHelper.java
+++ src/main/java/dev/imb11/flow/render/EMIHelper.java
@@ -1,14 +1,11 @@
 package dev.imb11.flow.render;
 
-import dev.emi.emi.EmiRenderHelper;
-import dev.emi.emi.runtime.EmiDrawContext;
-import dev.emi.emi.screen.EmiScreenBase;
-import dev.emi.emi.screen.EmiScreenManager;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 
 public class EMIHelper {
     public static void renderEMI(Screen screen, DrawContext context, int mouseX, int mouseY, float delta) {
-        EmiScreenManager.render(EmiDrawContext.wrap(context), mouseX, mouseY, delta);
+//        EmiScreenManager.render(EmiDrawContext.wrap(context), mouseX, mouseY, delta);
+        // TODO: Implement this in a future version.
     }
 }
