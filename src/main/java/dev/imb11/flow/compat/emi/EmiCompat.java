19fe0037c5d5ab3442efe1293b43c709aac9a709
Calum <contact@mineblock11.dev>
2024-08-18 20:50:47 +0100

    fix: compat entrypoint

--- src/main/java/dev/imb11/flow/compat/emi/EmiCompat.java
+++ src/main/java/dev/imb11/flow/compat/emi/EmiCompat.java
@@ -1,13 +1,12 @@
 package dev.imb11.flow.compat.emi;
 
-import com.mineblock11.mru.entry.CompatabilityEntrypoint;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EmiCompat implements CompatabilityEntrypoint {
+public class EmiCompat {
     public static final Logger LOGGER = LoggerFactory.getLogger("flow:emi");
     public static void displayExpandWarningToast() {
         LOGGER.info("Displaying EMI warning toast.");
@@ -18,10 +17,5 @@ public class EmiCompat implements CompatabilityEntrypoint {
         *//*?} else {*/
         client.getToastManager().add(new SystemToast(SystemToast.Type.WORLD_ACCESS_FAILURE, Text.translatable("flow.toast.emi.title"), Text.translatable("flow.toast.emi.description")));
         /*?}*/
-    }
-
-    @Override
-    public void initialize() {
-        LOGGER.info("Initialized EMI compatability.");
     }
 }
